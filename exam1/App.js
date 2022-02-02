a1723aa52ba70bbba1a05950f316697ff990a0bc
eunxoo <80334038+eunxoo@users.noreply.github.com>
2022-02-03 00:13:58 +0900

    exam1

---
+++ exam1/App.js
@@ -0,0 +1,24 @@
+import reactDom from "react-dom";
+//import "./App.css";
+import MyHeader from "./MyHeader";
+import Counter from "./Counter";
+import Container from "./Container";
+
+function App() {
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    initialValue: 5,
+  };
+  return (
+    <Container>
+      <div>
+        <MyHeader />
+        <Counter {...counterProps} />
+      </div>
+    </Container>
+  );
+}
+
+export default App;
