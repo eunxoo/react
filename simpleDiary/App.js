dbd846bbeb1f615d409e082cf3784058406b6d38
eunxoo <80334038+eunxoo@users.noreply.github.com>
2022-02-06 23:18:23 +0900

    배열 사용 -- 데이터 추가

--- simpleDiary/App.js
+++ simpleDiary/App.js
@@ -1,37 +1,53 @@
-//import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList.js";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "양은수",
-    content: "하이1",
-    emotion: 5,
-    create_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "김연경",
-    content: "하이2",
-    emotion: 4,
-    create_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "김준면",
-    content: "하이3",
-    emotion: 3,
-    create_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "양은수",
+//     content: "하이1",
+//     emotion: 5,
+//     create_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "김연경",
+//     content: "하이2",
+//     emotion: 4,
+//     create_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "김준면",
+//     content: "하이3",
+//     emotion: 3,
+//     create_date: new Date().getTime(),
+//   },
+// ];
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const create_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      create_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 };
