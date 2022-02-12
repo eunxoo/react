8fd5a93ac35e5405e988485fdeac50e9989fcca2
eunxoo <80334038+eunxoo@users.noreply.github.com>
2022-02-12 21:40:28 +0900

    최적화 - React.memo

--- simpleDiary/App.js
+++ simpleDiary/App.js
@@ -3,6 +3,7 @@ import React, { useState, useRef, useEffect, useMemo } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList.js";
+import OptimizeTest from "./OptimizeTest.js";
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -43,7 +44,6 @@ const App = () => {
   };
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가 삭제되었습니다`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
@@ -57,8 +57,6 @@ const App = () => {
   };
 
   const getDiaryAnalysis = useMemo(() => {
-    console.log("일기 분석 시작");
-
     const goodCount = data.filter((it) => it.emotion >= 3).length;
     const badCount = data.length - goodCount;
     const goodRatio = (goodCount / data.length) * 100;
@@ -68,6 +66,7 @@ const App = () => {
   const { goodCount, badCount, goodRatio } = getDiaryAnalysis; //함수가 아닌 값으로 변함
   return (
     <div className="App">
+      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
       <div>전체 일기 개수 : {data.length}</div>
       <div>기분 좋은 일기 개수 : {goodCount}</div>
