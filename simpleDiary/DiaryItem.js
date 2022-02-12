774252e166360b56b16064377cdd901c3a02acec
eunxoo <80334038+eunxoo@users.noreply.github.com>
2022-02-12 22:57:27 +0900

    최적화 완성

--- simpleDiary/DiaryItem.js
+++ simpleDiary/DiaryItem.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const DiaryItem = ({
   author,
@@ -74,4 +74,4 @@ const DiaryItem = ({
   );
 };
 
-export default DiaryItem;
+export default React.memo(DiaryItem);
