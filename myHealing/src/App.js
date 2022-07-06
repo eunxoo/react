52839a037a50b146f09a8097f59654b434d1356c
eunxoo <dmstn7989@naver.com>
2022-07-07 03:02:35 +0900

    register page 틀 구현

---
+++ myHealing/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Register from "./page/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
