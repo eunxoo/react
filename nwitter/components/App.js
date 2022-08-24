b7e2f200be90047b51b0c6729aa590096328d1a3
eunxoo <dmstn7989@naver.com>
2022-08-24 22:46:01 +0900

    setup

---
+++ nwitter/components/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import AppRouter from "./Router";
+function App() {
+  return <AppRouter></AppRouter>;
+}
+//https://nomadcoders.co/nwitter/lectures/1910
+export default App;
