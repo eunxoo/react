120cb78a949a940d477d6949474d53019cb0c823
eunxoo <dmstn7989@naver.com>
2022-08-25 19:17:22 +0900

    3.1 nweeting!
    cloud firestore
    위치 - asia northwest3(seoul)

--- nwitter/routes/Home.js
+++ nwitter/routes/Home.js
@@ -1,4 +1,49 @@
-import React from "react";
+import { dbService } from "fbase";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"), {
+        nweet,
+        createAt: Date.now(),
+      });
+      console.log("Document writeen with ID : ", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+
+    setNweet("");
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          type="text"
+          placeholder="What's on your mind?"
+          onChange={onChange}
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div></div>
+    </div>
+  );
+};
 export default Home;
