e6679b558fe86dab4cda5ccaf1f98bdec9bb5379
eunxoo <dmstn7989@naver.com>
2022-08-25 21:36:27 +0900

    3.4 Delete and Update part One
    로그인 할 때 받았던 값과 글을 쓸 때 받은 값 (아이디) 비교해서 boolean 값으로 버튼 유무

--- nwitter/routes/Home.js
+++ nwitter/routes/Home.js
@@ -1,3 +1,4 @@
+import Nweet from "components/Nweet";
 import { dbService } from "fbase";
 import {
   getDocs,
@@ -60,9 +61,11 @@ const Home = ({ userObj }) => {
       </form>
       <div>
         {nweets.map((nweet) => (
-          <div key={nweet.id}>
-            <h4>{nweet.text}</h4>
-          </div>
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.createorId === userObj.uid}
+          />
         ))}
       </div>
     </div>
