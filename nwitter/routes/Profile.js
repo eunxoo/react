a72ac8ef704a9450365b08018e9a3f5e4ee7b312
eunxoo <dmstn7989@naver.com>
2022-08-26 17:32:14 +0900

    5.0 Get My Own Nweets
    my page에 들어가면 콘솔에 index 에러가 뜸. 나오는 링크 누르면 자동으로 firebase에 들어가서 인덱스 처리 해줌 굿!!

--- nwitter/routes/Profile.js
+++ nwitter/routes/Profile.js
@@ -1,13 +1,38 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "fbase";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+} from "@firebase/firestore";
 
-export default () => {
+const Profile = ({ userObj }) => {
   const onLogOutClick = () => {
     authService.signOut();
   };
+
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt", "desc")
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, " => ", doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
 };
+
+export default Profile;
